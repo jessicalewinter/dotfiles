27de1be85d9a6c87b75fadd37b291688496bf58e
Jessica Lewinter <jessicalewinter@gmail.com>
2022-12-20 23:10:08 -0300

    delete: remove iTerm app

--- iTerm.app/Contents/Resources/iterm2Invoke.js
+++
@@ -1,9 +0,0 @@
-function iterm2Invoke(invocation, callback) {
-    var messgeToPost = {'invocation': invocation,
-                        'callback': callback};
-    window.webkit.messageHandlers.iterm2Invoke.postMessage(messgeToPost);
-}
-window.onerror = function(msg) {
-    // Sends a message to the app to log the message to the console.
-    alert(msg);
-};
